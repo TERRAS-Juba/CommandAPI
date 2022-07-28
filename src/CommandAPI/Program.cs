b5acef61493d124da17d6b146abfee1920fd154e
TERRAS Juba <ij_terras@esi.dz>
2022-07-28 09:36:26 +0200

    Add SqlCommandAPIRepo.cs

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services and dependancies
 builder.Services.AddControllers();
 builder.Services.AddDbContext<CommandContext>(o=>o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
-builder.Services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
 var app = builder.Build();
 
 // Add middlewares
