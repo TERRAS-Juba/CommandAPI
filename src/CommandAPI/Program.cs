e5ce2f94f8d88ed8876e083773d85409b715321c
TERRAS Juba <ij_terras@esi.dz>
2022-07-28 10:55:57 +0200

    Remove sensitive informations from appsetting.json

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using CommandAPI.Data;
+using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services and dependancies
 builder.Services.AddControllers();
-builder.Services.AddDbContext<CommandContext>(o=>o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+var connectionBuilder=new NpgsqlConnectionStringBuilder();
+connectionBuilder.ConnectionString=builder.Configuration.GetConnectionString("PostgreSqlConnection");
+connectionBuilder.Password=builder.Configuration["Password"];
+connectionBuilder.Username=builder.Configuration["UserID"];
+builder.Services.AddDbContext<CommandContext>(o=>o.UseNpgsql(connectionBuilder.ConnectionString));
 builder.Services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
 var app = builder.Build();
 
