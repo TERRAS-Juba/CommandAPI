b642bcb2af789c03387bb446a0220c74855db58a
TERRAS Juba <ij_terras@esi.dz>
2022-07-28 09:13:50 +0200

    Add migration 'AddCommandToDb'

--- src/CommandAPI/Data/CommandContext.cs
+++ src/CommandAPI/Data/CommandContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using CommandAPI.Models;
 namespace CommandAPI.Data
 {
+
     public class CommandContext : DbContext
     {
         public CommandContext(DbContextOptions<CommandContext> options)
--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using CommandAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services and dependancies
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CommandContext>(o=>o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 builder.Services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
 var app = builder.Build();
 
