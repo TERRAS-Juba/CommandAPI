06411ce2f4babfa980b3323513676181bc1479d5
TERRAS Juba <ij_terras@esi.dz>
2022-07-29 10:13:00 +0200

    GET, POST, PUT, PATCH, DELETE

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,16 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using CommandAPI.Data;
 using Npgsql;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services and dependancies
-builder.Services.AddControllers();
-var connectionBuilder=new NpgsqlConnectionStringBuilder();
-connectionBuilder.ConnectionString=builder.Configuration.GetConnectionString("PostgreSqlConnection");
-connectionBuilder.Password=builder.Configuration["Password"];
-connectionBuilder.Username=builder.Configuration["UserID"];
-builder.Services.AddDbContext<CommandContext>(o=>o.UseNpgsql(connectionBuilder.ConnectionString));
-builder.Services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
+builder.Services.AddControllers().AddNewtonsoftJson(s =>
+{
+    s.SerializerSettings.ContractResolver = new
+    CamelCasePropertyNamesContractResolver();
+});
+var connectionBuilder = new NpgsqlConnectionStringBuilder();
+connectionBuilder.ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+connectionBuilder.Password = builder.Configuration["Password"];
+connectionBuilder.Username = builder.Configuration["UserID"];
+builder.Services.AddDbContext<CommandContext>(o => o.UseNpgsql(connectionBuilder.ConnectionString));
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Add middlewares
